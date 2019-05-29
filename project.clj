66742ebfa98d2e5e4d2f5d9f4ada566ddfe7b517
Simon Brooke <simon@journeyman.cc>
2019-05-29 13:35:04 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject the-great-game "0.1.0-SNAPSHOT"
+(defproject the-great-game "0.1.0"
   :cloverage {:output "docs/cloverage"}
   :codox {:metadata {:doc "**TODO**: write docs"
                      :doc/format :markdown}
