395c899a627754e363ffa9e13caaa479274a2c7d
Simon Brooke <simon@journeyman.cc>
2024-04-04 13:26:51 +0100

    Upversioned for further development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject journeyman-cc/the-great-game "0.1.2-SNAPSHOT"
+(defproject journeyman-cc/the-great-game "0.1.3-SNAPSHOT"
   :cloverage {:output "docs/cloverage"
               :codecov? true
               :emma-xml? true}
